22f9c51e67f590e8c0313eb59e32f5fd0306c3fb
Le Duc Léandre <leandre.leduc38@gmail.com>
2026-02-01 23:00:22 +0100

    refactor: Move circular queue for round robin in a separate file for clarity
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

---
+++ src/circ_queue.c
@@ -0,0 +1,48 @@
+#include "circ_queue.h"
+#include "process.h"
+#include <stddef.h>
+
+/** Get the active process **/
+inline process_t *peek_head(circ_queue_t *q) {
+  if (q->size == 0)
+    return NULL;
+  return q->queue[q->head];
+}
+
+/** Round Robin circular gestion of a queue **/
+void rotate_head_to_tail(circ_queue_t *q) {
+  // Nothing to do if 0 or 1 element
+  if (q->size <= 1)
+    return;
+
+  process_t *p = q->queue[q->head];
+  q->queue[q->tail] = p;
+
+  q->tail = (q->tail + 1) % MAX_PROC;
+  q->head = (q->head + 1) % MAX_PROC;
+}
+
+/** Add a new process to a circular queue **/
+int enqueue(circ_queue_t *q, process_t *proc) {
+  if (q->size >= MAX_PROC)
+    return -1; // queue is full
+
+  q->queue[q->tail] = proc;
+  q->tail = (q->tail + 1) % MAX_PROC;
+  q->size++;
+
+  return 0;
+}
+
+/** Remove the head process from a circular queue **/
+int dequeue(circ_queue_t *q) {
+  if (q->size == 0)
+    return -1;
+
+  q->head = (q->head + 1) % MAX_PROC;
+  q->size--;
+
+  return 0;
+}
+
+uint8_t is_empty(circ_queue_t *q) { return q->size == 0; }
