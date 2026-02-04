867767e02dfe1785d4dc9d50ab802ceaeeff0d06
Le Duc Léandre <leandre.leduc38@gmail.com>
2026-02-04 12:00:09 +0100

    refactor: irq relative files organisation, separating time and irq
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

--- src/time.h
+++ src/time.h
@@ -1,10 +1,7 @@
 #pragma once
 #include <stdint.h>
 
-// Timer irq enable/disable
-extern void enable_timer();
-extern void disable_timer();
+uint32_t seconds();
+void init_timer();
 
-extern void trap_handler(uint64_t mcause, uint64_t mie, uint64_t mip);
-extern uint32_t seconds();
-extern void init_traitant(void traitant());
+void timer_irq_handler(void);
