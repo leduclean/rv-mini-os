bc723478f9f1cf6f95f097937ab10bcc3734fab3
Le Duc Léandre <leandre.leduc38@gmail.com>
2026-02-04 15:50:24 +0100

    fix(types): Convert adress into uintptr before deferencing in MMIO macro
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

--- src/mmio.h
+++ src/mmio.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Macro definition for memory mapped container
-#define MMIO8(addr) (*(volatile uint8_t *)(addr))
-#define MMIO16(addr) (*(volatile uint16_t *)(addr))
-#define MMIO32(addr) (*(volatile uint32_t *)(addr))
+#define MMIO8(addr) (*(volatile uint8_t *)(uintptr_t)(addr))
+#define MMIO16(addr) (*(volatile uint16_t *)(uintptr_t)(addr))
+#define MMIO32(addr) (*(volatile uint32_t *)(uintptr_t)(addr))
 #define MMIO64(addr) (*(volatile uint64_t *)(uintptr_t)(addr))
