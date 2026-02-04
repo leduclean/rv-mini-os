b7585496ab7cd7a6d30d8ae03db25881f38097a9
Le Duc Léandre <leandre.leduc38@gmail.com>
2026-02-04 12:08:42 +0100

    feat: Add Machine time external irq enable/disable helpers
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

--- src/irq.h
+++ src/irq.h
@@ -8,3 +8,7 @@ void trap_handler(uint64_t mcause, uint64_t mie, uint64_t mip);
 // Timer interrupts
 void disable_timer();
 void enable_timer();
+
+// External interrupts
+void enable_external();
+void disable_external();
