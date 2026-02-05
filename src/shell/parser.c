98fbaf13e1faa392029c3bb332ced95a3c2d6e45
Le Duc Léandre <leandre.leduc38@gmail.com>
2026-02-05 13:38:25 +0100

    feat(shell): Mini Shell chore functions implementation ie parsing/handling
    Basically a little shell process with cmd line parsing into cmd and execution.
    `help` function is available but we will implement `ps` after dynamical
    allocation implementation.
    
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

---
+++ src/shell/parser.c
@@ -0,0 +1,65 @@
+#include "parser.h"
+#include "console.h"
+#include "scheduler.h"
+#include "stdio.h"
+#include "uart.h"
+#include <string.h>
+
+/* Buffer flush */
+static void flush(char *buf) { memset(&buf, 0, sizeof(buf)); }
+
+/** Processus that echo the uart character and write in the buffer
+ * the current line **/
+int parser_read_line(char *buf) {
+  flush(buf);
+  int len = 0;
+  for (;;) {
+    char c;
+    while (uart_read(&c) == -1) {
+      scheduler_block_on(uart_get_wait_queue());
+    }
+
+    if ((c == '\n') || (c == '\r')) {
+      buf[len] = '\0';
+      printf("\n");
+      return len;
+    }
+
+    if (c == '\b' || c == 127) { // backspace
+      if (len > 0) {
+        len--;
+        printf("\b \b");
+      }
+      continue;
+    }
+
+    if (len < MAX_COLS - 1) {
+      buf[len++] = c;
+      // Echo the current character
+      printf("%c", c);
+    }
+  }
+}
+
+/** Tokenize the line and convert it into shell command **/
+shell_cmd_desc_t parser_get_cmd(char *buf) {
+  shell_cmd_desc_t cmd;
+  cmd.argc = 0;
+  char *token = NULL;
+  token = strtok(buf, " ");
+  while (token && (cmd.argc < MAX_ARGS)) {
+    cmd.argv[cmd.argc++] = token;
+    token = strtok(NULL, " ");
+  }
+
+  return cmd;
+}
+
+/** If last cmd argument is & then set backround to true and remove the last arg
+ * **/
+void check_background(shell_cmd_desc_t *cmd) {
+  if ((cmd->argc > 0) && strcmp(cmd->argv[cmd->argc - 1], "&") == 0) {
+    cmd->background = 1;
+    cmd->argc--;
+  }
+}
--- src/start.c
+++ src/start.c
@@ -1,50 +1,11 @@
 #include "console.h"
 #include "irq.h"
 #include "process.h"
-#include "scheduler.h"
-#include "time.h"
+#include "shell/shell.h"
 #include "uart.h"
 #include <stddef.h>
-#include <stdio.h>
 
 extern void mon_traitant(void);
-
-void proc1() {
-  for (int i = 0; i < 2; i++) {
-    printf("[temps = %u] processus %s pid = %i\n", seconds(), get_active_name(),
-           get_active_pid());
-    scheduler_sleep(2);
-  }
-}
-
-void proc3() {
-  for (;;) {
-    printf("[temps = %u] processus %s pid = %i\n", seconds(), get_active_name(),
-           get_active_pid());
-    scheduler_sleep(1);
-  }
-}
-
-void proc2() {
-  for (int i = 0; i < 10; i++) {
-    printf("[temps = %u] processus %s pid = %i\n", seconds(), get_active_name(),
-           get_active_pid());
-    if (i == 1) {
-      spawn_process(proc3, "bob3", HIGH);
-    }
-    scheduler_sleep(3);
-  }
-}
-
-void console_reader() {
-  for (;;) {
-    char c;
-    while (uart_read(&c) == -1) {
-      scheduler_block_on(uart_get_wait_queue());
-    }
-    printf("%c", c);
-  }
-}
 
 void kernel_start() {
   // Plic config
@@ -57,8 +18,8 @@ void kernel_start() {
   enable_external();
   enable_timer();
 
-  spawn_process(console_reader, "console", NORMAL);
-  spawn_process(proc1, "bob1", HIGH);
-  spawn_process(proc2, "bob2", NORMAL);
+  spawn_process(shell, "shell", NORMAL);
+  // spawn_process(proc1, "bob1", HIGH);
+  // spawn_process(proc2, "bob2", NORMAL);
   idle();
 }
