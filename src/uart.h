44430a826a568b92f26ad01de6b56582325b7a73
Le Duc Léandre <leandre.leduc38@gmail.com>
2026-02-06 23:41:04 +0100

    refactor: Wait and Block queue API using chained list
    Since a process is always in a specific queue depending on it state,
    we can use a single pointer for each of the queue.
    Doing so, instead of static allocated circ queue we got one single pointer for all the queues.
    
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

--- src/uart.h
+++ src/uart.h
@@ -1,8 +1,8 @@
 #pragma once
-#include "circ_queue.h"
+#include "process.h"
 
 void uart_init();
-circ_queue_t *uart_get_wait_queue();
+wait_queue_t *uart_get_wait_queue();
 void uart_putchar(char);
 void uart_irq_handler();
 int uart_read(char *c);
