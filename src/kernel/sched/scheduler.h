27c9490df00be22cc0f78bf89c6021066e5c5e3a
Le Duc Léandre <leandre.leduc38@gmail.com>
2026-02-08 15:41:09 +0100

    refactor: Remove uneeded extern from functions declarations in headers
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

--- src/kernel/sched/scheduler.h
+++ src/kernel/sched/scheduler.h
@@ -3,14 +3,14 @@
 #include "kernel/process/process.h"
 #include "lib/stdint.h"
 
-extern void scheduler_rotate();
+void scheduler_rotate();
 
-extern void scheduler_admit(process_t *proc);
+void scheduler_admit(process_t *proc);
 
 void scheduler_ready_process(process_t *proc);
 
 // Sleeping queue functions
-extern void scheduler_wake_sleeping();
+void scheduler_wake_sleeping();
 void scheduler_wake_waiting_queue(wait_queue_t *wq);
 void remove_from_sleeping(process_t *proc);
 uint8_t is_in_sleeping_queue(process_t *proc);
@@ -20,7 +20,7 @@ void scheduler_block_on(wait_queue_t *wq);
 void scheduler_block_on_with_timeout(wait_queue_t *wq, uint32_t timeout_secs);
 
 // Scheduling function
-extern void init_scheduler_queues();
-extern void scheduler_sleep(uint32_t nbr_secondes);
-extern void scheduler_terminate();
-extern void proc_launcher(void proc());
+void init_scheduler_queues();
+void scheduler_sleep(uint32_t nbr_secondes);
+void scheduler_terminate();
+void proc_launcher(void proc());
