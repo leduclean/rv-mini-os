031d8e4402b42f603f4c4d337b019e2ce4080e1d
Le Duc Léandre <leandre.leduc@etik.com>
2026-09-04 02:44:47 +0200

    refactor: move the memory layout to the kernel itself
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

---
+++ src/kernel/core/memory/memory.c
@@ -0,0 +1,10 @@
+#include "memory.h"
+#include "csr.h"
+
+void pmp_allow_all()
+{
+	//NOTE: We use the whole memory for the process to enter user mode
+	csr_write(pmpaddr0, ~0UL);
+	// NOTE: this mode is a stub to enter user mode and smoke test the user mode
+	csr_write(pmpcfg0, PMP_A_NAPOT | PMP_R_BIT | PMP_W_BIT | PMP_X_BIT);
+}
