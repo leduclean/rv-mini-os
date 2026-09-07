2a21b223fd7f38d33e52b37762f51043b8818fe6
Le Duc Léandre <leandre.leduc@etik.com>
2026-09-07 15:17:28 +0200

    feat(memory): add memory mapping implem for kernel and for a user proc
    The whole kernel code is identity mapped in S mode.
    The user code is also identity mapped for now.
    
    Both of the kernel and the user code tables share a common
    TRAMPOLINE virtual address used to handle satp switching in a
    common and shared code.
    
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

---
+++ src/kernel/core/memory/mmap.c
@@ -0,0 +1,132 @@
+#include "mmap.h"
+#include "pages.h"
+#include "csr.h"
+#include "platform.h"
+#include "process.h"
+#include "vpages.h"
+#include "asm_defs.h"
+#include "minilib/stdio.h"
+
+/* Linker symbols needed for mapping */
+extern char _etext[], _edata[], _user_start[], _user_end[], _trampoline_start[],
+	_trampoline_end[], _ram_start[], _end[];
+
+pte_t *kroot;
+
+#define SATP_SV39_MODE 8UL
+#define SATP_MODE_SHIFT 60
+#define SATP_MODE_MASK (0xfUL << SATP_MODE_SHIFT)
+
+unsigned long get_satp(void *pt)
+{
+	return ((SATP_SV39_MODE << SATP_MODE_SHIFT) | PAGE_NUMBER(pt));
+}
+
+unsigned long get_kernel_satp()
+{
+	return get_satp(kroot);
+}
+
+int map_kernel()
+{
+	int res;
+	kroot = page_alloc();
+	if (!kroot) {
+		return -1;
+	}
+
+	res = map_range(kroot, (void *)_ram_start, (void *)_ram_start,
+			(size_t)_heap_end - (size_t)_ram_start,
+			PTE_R | PTE_W | PTE_X);
+	if (res < 0) {
+		goto err_free_root;
+	}
+
+	res = map_range(kroot, (void *)TRAMPOLINE, _trampoline_start,
+			_trampoline_end - _trampoline_start, PTE_X | PTE_R);
+	if (res < 0) {
+		goto err_free_root;
+	}
+
+	res = map_range(kroot, (void *)UART_BASE, (void *)UART_BASE, PAGE_SIZE,
+			PTE_R | PTE_W);
+	if (res < 0) {
+		goto err_free_root;
+	}
+
+	res = map_range(kroot, (void *)BOCHS_DISPLAY_BASE_ADDRESS,
+			(void *)BOCHS_DISPLAY_BASE_ADDRESS, DISPLAY_BYTES,
+			PTE_R | PTE_W);
+	if (res < 0) {
+		goto err_free_root;
+	}
+
+	res = map_range(kroot, (void *)PLIC_MMIO_BASE, (void *)PLIC_MMIO_BASE,
+			PLIC_MMIO_SIZE, PTE_R | PTE_W);
+	if (res < 0) {
+		goto err_free_root;
+	}
+
+	// Alow Sv39 and write the kroot PPN to satp.
+	csr_write(satp, get_satp(kroot));
+	update_tlb();
+
+	return 0;
+
+err_free_root:
+	page_free(kroot);
+	return res;
+}
+
+int map_uprocess(process_t *p)
+{
+	printf("[INFO]: mapping user process %s \n", p->name);
+
+	if (!p->user) {
+		return 0;
+	}
+
+	pte_t *root = p->root_ptable;
+	int res;
+
+	// The whole code is identity mapped for now.
+	res = map_urange(root, _user_start, _user_start,
+			 _user_end - _user_start, PTE_X | PTE_R);
+	if (res < 0) {
+		return res;
+	}
+
+	void *stack = page_alloc();
+	if (!stack) {
+		return -1;
+	}
+	map_upage(root, (void *)USTACK_TOP, stack, PTE_R | PTE_W);
+
+	void *tframe = page_alloc();
+	if (!tframe) {
+		res = -1;
+		goto err_free_stack;
+	}
+	res = map_page(root, (void *)TRAPFRAME, tframe, PTE_R | PTE_W);
+	if (res < 0) {
+		goto err_free_tframe;
+	}
+	p->tframe = tframe;
+
+	// This is not U page because we will go back to user page
+	// in S mode before the sret returns to user mode.
+	res = map_range(root, (void *)TRAMPOLINE, _trampoline_start,
+			_trampoline_end - _trampoline_start, PTE_X | PTE_R);
+	if (res < 0) {
+		goto err_free_tframe;
+	}
+
+	printf("[INFO]: process %s mapped \n", p->name);
+	return 0;
+
+err_free_tframe:
+	page_free(tframe);
+err_free_stack:
+	page_free(stack);
+	return res;
+}
