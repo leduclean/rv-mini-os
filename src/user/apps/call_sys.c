7332a579151dd23ce2b8c2a9211661eb8d54cf61
Le Duc Léandre <leandre.leduc@etik.com>
2026-09-09 17:13:18 +0200

    refactor: rename progs to app for user programs
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

--- src/user/apps/call_sys.c
+++ src/user/apps/call_sys.c
@@ -1,13 +1,10 @@
 #include "syscall.h"
-#include "progs.h"
+#include "apps.h"
 #include "minilib/stdio.h"
 
 #define SLEEP_TIME 2
 
 __attribute__((section(".user_text"))) void sleep_call()
 {
-	volatile int x = 42;
-	for (;;) {
-		sleep(SLEEP_TIME);
-	}
+	sleep(SLEEP_TIME);
 }
