54315aff35055acc270cbc44a96e2d6d86c477d6
Le Duc Léandre <leandre.leduc@etik.com>
2026-09-09 17:14:30 +0200

    feat: add a tree_free that release all the allocated pages of a process
    This methode prepare the process clean phase that will append
    after an exit() syscall.
    
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

--- src/kernel/core/memory/vpages.c
+++ src/kernel/core/memory/vpages.c
@@ -1,5 +1,7 @@
 #include "vpages.h"
+#include "asm_defs.h"
 #include "minilib/stddef.h"
+#include "mmap.h"
 #include "pages.h"
 
 /* Sv39 indirection levels */
@@ -15,6 +17,8 @@
 
 /* PTE PPN first indirection shift */
 #define PTE_PPN0_SHIFT 10
+#define PTE_SHIFT 3
+#define PTE_ENTRY_PER_TABLE (PAGE_SIZE >> PTE_SHIFT)
 
 /**
  * @brief Helper to get a lvl of indirection of vpn given a va.
@@ -47,6 +51,16 @@ static inline unsigned long _get_va_vpn(const void *va, uint8_t lvl)
 	return (addr & mask) >> shift;
 }
 
+static inline pte_t *_get_pte(pte_t *table, const void *va, int lvl)
+{
+	return &table[_get_va_vpn(va, lvl)];
+}
+
+static inline pte_t *_get_next_lvl_pte(const pte_t pte)
+{
+	return (pte_t *)((pte >> PTE_PPN0_SHIFT) << PAGE_SHIFT);
+}
+
 int map_page(pte_t *root, const void *va, void *pa, unsigned long flags)
 {
 	pte_t *table = root;
@@ -56,7 +70,7 @@ int map_page(pte_t *root, const void *va, void *pa, unsigned long flags)
 	int error_lvl = -1;
 
 	for (int lvl = LEVELS - 1; lvl > 0; lvl--) {
-		pte_t *pte = &table[_get_va_vpn(va, lvl)];
+		pte_t *pte = _get_pte(table, va, lvl);
 
 		if (!(*pte & PTE_V)) {
 			void *next = page_alloc();
@@ -69,12 +83,12 @@ int map_page(pte_t *root, const void *va, void *pa, unsigned long flags)
 			alloc_tables[lvl] = next;
 			*pte = (PAGE_NUMBER(next) << PTE_PPN0_SHIFT) | PTE_V;
 		}
+
 		// Go to the next level of indirection
-		table = (pte_t *)((*pte >> PTE_PPN0_SHIFT) << PAGE_SHIFT);
+		table = _get_next_lvl_pte(*pte);
 	}
 
-	pte_t *leaf_pte = &table[_get_va_vpn(va, 0)];
-
+	pte_t *leaf_pte = _get_pte(table, va, 0);
 	unsigned long pte_flags = flags | PTE_V | PTE_A;
 
 	if (flags & PTE_W)
@@ -92,6 +106,49 @@ err_release_prv:
 
 	return LEVELS - 1 - error_lvl;
 }
+
+static inline bool _is_a_next_lvl_ptr(pte_t pte)
+{
+	return (!(pte & PTE_X) && !(pte & PTE_W) && !(pte & PTE_R));
+}
+
+/**
+ * @brief Recursive free walk to the sv39 entries.
+ *
+ * @param table The root table of the walk.
+ * @param lvl The number of indirection lvl in the walk.
+ */
+static void walk_free(pte_t *table, int lvl)
+{
+	for (int i = 0; i < PTE_ENTRY_PER_TABLE; i++) {
+		pte_t pte = table[i];
+
+		if (pte & PTE_V) {
+			if (lvl > 0 && _is_a_next_lvl_ptr(pte)) {
+				// Go to the next lvl and free all entry too
+				walk_free(_get_next_lvl_pte(pte), lvl - 1);
+			} else {
+				// Leaf pte
+				if (pte & (PTE_V) && !_is_a_next_lvl_ptr(pte)) {
+					void *page = _get_next_lvl_pte(pte);
+					page_free((void *)page);
+				}
+				// Reset the entry
+				table[i] = 0;
+			}
+		}
+	}
+	// Free the table itself
+	page_free(table);
+}
+
+void tree_free(pte_t *root)
+{
+	if (!root) {
+		return;
+	}
+	walk_free(root, LEVELS - 1);
+};
 
 int map_range(pte_t *root, void *va, void *pa, size_t size, unsigned long flags)
 {
