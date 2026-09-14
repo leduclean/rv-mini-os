dc37a32e5df4d6212bccb235d2c0efab1d8f6a02
Le Duc Léandre <leandre.leduc@etik.com>
2026-09-14 16:56:25 +0200

    chore: add the ffreestanding option to indicate no libc used
    Signed-off-by: Le Duc Léandre <leandre.leduc@etik.com>

--- src/riscv64-toolchain.cmake
+++ src/riscv64-toolchain.cmake
@@ -5,6 +5,6 @@ set(CMAKE_C_COMPILER   riscv64-elf-gcc)
 set(CMAKE_CXX_COMPILER riscv64-elf-g++)
 set(CMAKE_ASM_COMPILER riscv64-elf-gcc)
 
-set(CMAKE_C_FLAGS   "-std=c17 -march=rv64ima_zicsr -mabi=lp64 -mcmodel=medany -Wall -Wextra -O0 -ggdb3 -ffunction-sections -fdata-sections")
+set(CMAKE_C_FLAGS   "-std=c17 -march=rv64ima_zicsr -mabi=lp64 -mcmodel=medany -Wall -Wextra -O0 -ggdb3 -ffunction-sections -fdata-sections -ffreestanding")
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS "-nostdlib -static -lgcc -Wl,--nmagic -Wl,--gc-sections -g")
